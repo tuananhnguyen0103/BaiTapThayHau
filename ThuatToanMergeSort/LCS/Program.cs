c2dcdd4298bd5626a48481964e3cbc5f52e2fe96
tuananh010320 <tuananh010320@gmail.com>
2020-10-17 14:28:56 +0700

    update 5: LCS complete

--- ThuatToanMergeSort/LCS/Program.cs
+++ ThuatToanMergeSort/LCS/Program.cs
@@ -9,53 +9,70 @@ namespace LCS
     class Program
     {
         /* Returns length of LCS for X[0..m-1], Y[0..n-1] */
-        static int lcs(char[] X, char[] Y, int m, int n)
+        static void lcs(string S1, string S2)
         {
-            int[,] L = new int[m + 1, n + 1];
+            // S1 -> Row, S2 -> Col
+            int lengtS1 = S1.Length, lengtS2 = S2.Length;
 
-            /* Following steps build L[m+1][n+1]  
-            in bottom up fashion. Note 
-            that L[i][j] contains length of  
-            LCS of X[0..i-1] and Y[0..j-1] */
-            Console.WriteLine(m+1);
-            Console.WriteLine(n+1);
-            for (int i = 0; i <= m; i++)
+            int[,] LCS = new int[lengtS1+1, lengtS2+1];
+
+            for (int i = 0; i <= lengtS1; i++)
             {
-                for (int j = 0; j <= n; j++)
+                for (int j = 0; j <= lengtS2; j++)
                 {
-                    if (i == 0 || j == 0)
-                        L[i, j] = 0;
-                    else if (X[i - 1] == Y[j - 1])
-                        L[i, j] = L[i - 1, j - 1] + 1;
+                    if (i == 0|| j == 0)
+                    {
+                        LCS[i, j] = 0;
+                    }
+                    else if (S1[i - 1] == S2[j - 1])
+                    {
+                        LCS[i, j] = LCS[i - 1, j - 1] + 1;
+                    }
                     else
-                        L[i, j] = max(L[i - 1, j], L[i, j - 1]);
+                    {
+                        LCS[i, j] = Math.Max(LCS[i - 1, j], LCS[i, j - 1]);
+                    }
                 }
             }
-            return L[m, n];
+
+            int index = LCS[lengtS1, lengtS2];
+            char[] result = new char[index];
+            while(index != 0)
+            {
+                if (S1[lengtS1 - 1] == S2[lengtS2 - 1])
+                {
+                    result[index - 1] = S1[lengtS1 - 1];
+                    index--;lengtS1--;lengtS2--;
+                }
+                else
+                {
+                    if (LCS[lengtS1 - 1, lengtS2] > LCS[lengtS1, lengtS2 - 1])
+                    {
+                        lengtS1--;
+                    }
+                    else
+                    {
+                        lengtS2--;
+                    }
+                }
+            }
+            foreach (var item in result)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine();
         }
 
         /* Utility function to get max of 2 integers */
-        static int max(int a, int b)
-        {
-            return (a > b) ? a : b;
-        }
+        
 
         // Driver code 
         public static void Main()
         {
 
-            String s1 = "Vu Chung Dung";
-            String s2 = "Nguyen Tuan Anh";
-
-            char[] X = s1.ToCharArray();
-            char[] Y = s2.ToCharArray();
-            int m = X.Length;
-            int n = Y.Length;
-            Console.WriteLine(s1);
-            Console.WriteLine(s2);
-            Console.WriteLine(m);
-            Console.WriteLine(n);
-            Console.Write("Do dai cua chuoi la" + " " + lcs(X, Y, m, n));
+            string S1 = "asdasdq";
+            string S2 = "anisdacq";
+            lcs(S1, S2);
             Console.ReadKey();
         }
     }
--- ThuatToanMergeSort/LongestCommomSubSequence/Program.cs
+++ ThuatToanMergeSort/LongestCommomSubSequence/Program.cs
@@ -8,15 +8,71 @@ namespace LongestCommomSubSequence
 {
     class Program
     {
-        static int LCS(char[] S1 , char[] S2,int m,int n)
+        static void LCS(string str1, string str2)
         {
-            int[,] LongCS = new int[m, n];
+            // Độ dài của 2 chuỗi
+            int lengStr1 = str1.Length;
+            int lengStr2 = str2.Length;
+            
+            // Tạo ma trận có chiều bằng độ dài của 2 chuỗi cộng thêm 1
+            int[,] LCSelements = new int[lengStr1 + 1, lengStr2 + 1];
 
-            return LongCS[m, n];
+            // Dùng 2 vòng for để gán giá trị cho 
+            for (int i = 0; i <= lengStr1; i++)
+            {
+                for (int j = 0; j <= lengStr2; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        LCSelements[i, j] = 0;
+                    }
+                    else if (str1[i - 1] == str2[j - 1])
+                    {
+                        LCSelements[i, j] = LCSelements[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        LCSelements[i, j] = Math.Max(LCSelements[i - 1, j], LCSelements[i, j - 1]);
+                    }
+                }
+            }
+
+            
+            int index = LCSelements[lengStr1, lengStr2];
+            char[] LCSchar = new char[index];
+            int strRow = lengStr1, strCol = lengStr2;
+            while (strCol > 0 && strRow > 0)
+
+            {
+                if (str1[strRow - 1] == str2[strCol - 1])
+                {
+                    LCSchar[index-1] = str1[strRow - 1];
+                    strRow--;strCol--;index--;
+                }
+                else
+                {
+                    if (LCSelements[strRow - 1, strCol] > LCSelements[strRow, strCol - 1])
+                    {
+                        strRow--;
+                    }
+                    else
+                    {
+                        strCol--;
+                    }
+                }
+            }
+            foreach (var item in LCSchar)
+            {
+                Console.Write(item+"\t");
+            }
         }
         static void Main(string[] args)
         {
+            string str1 = "adcdf";
+            string str2 = "badef";
+            LCS(str1, str2);
             Console.ReadKey();
+
         }
     }
 }
